f9fd6289e829ff8db878abb8f732d92156654ce3
Benjamin Bloomfield <bhb123+g@gmail.com>
2013-08-13 10:50:58 -0400

    Added some rubrics and fixed some mistakes.

--- compline.js
+++ compline.js
@@ -1,32 +1,34 @@
 $(function(){
+  $.QueryString = (function (a) {
+      if (a == "") return {};
+      var b = {};
+      for (var i = 0; i < a.length; ++i) {
+          var p = a[i].split('=');
+          if (p.length != 2) continue;
+          b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+      }
+      if (b.successMsg) showAlert(false, b.successMsg);
+      if (b.failMsg) showAlert(true, b.failMsg);
+      return b;
+  })(window.location.search.substr(1).split('&'));
   var ant = "<div class='jgabc' src='psalms/{day}/ant.gabc'></div>";
   var psalm = "<div class='jgabc' src='psalms/{day}/psalm.gabc'></div>"
   var date = new Date();
   var day = date.getDay();
   var dayName;
-  switch(day){
-    case 0:
-      dayName = 'Sunday';
-      break;
-    case 1:
-      dayName = 'Monday';
-      break;
-    case 2:
-      dayName = 'Tuesday';
-      break;
-    case 3:
-      dayName = 'Wednesday';
-      break;
-    case 4:
-      dayName = 'Thursday';
-      break;
-    case 5:
-      dayName = 'Friday';
-      break;
-    case 6:
-      dayName = 'Saturday';
-      break;
+  var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  if($.QueryString.day) {
+    var test = $.QueryString.day;
+    if(test in days) {
+      day = test;
+    } else {
+      test = days.indexOf(test);
+      if(test in days) {
+        day = test;
+      }
+    }
   }
+  dayName = days[day];
   $('#weekday').text(dayName);
   $.get('psalms/'+day+'/psalm-verses.html',function(data){
     var html = ant + psalm + data + ant;
@@ -34,7 +36,7 @@ $(function(){
     $('#placeholder').replaceWith(html);
     updateGabc();
   });
-  $('#in-manus-tuas-choices input,#te-lucis-choices input,#marian-antiphon-choices input').change(function(){
+  $('#in-manus-tuas-choices input,#te-lucis-choices input,#marian-antiphon-choices input,#confiteor-choices input').change(function(){
     var chant = this.name;
     var src = chant + '/' + this.value + '.gabc';
     console.info(src);
