7b39c797988a05815f5b84d63842d3211a5beca8
Benjamin Bloomfield <bhb123+g@gmail.com>
2013-08-14 10:13:51 -0400

    Fixed some omissions and typos, and added the option to select Paschal Time

--- compline.js
+++ compline.js
@@ -11,8 +11,6 @@ $(function(){
       if (b.failMsg) showAlert(true, b.failMsg);
       return b;
   })(window.location.search.substr(1).split('&'));
-  var ant = "<div class='jgabc' src='psalms/{day}/ant.gabc'></div>";
-  var psalm = "<div class='jgabc' src='psalms/{day}/psalm.gabc'></div>"
   var date = new Date();
   var day = date.getDay();
   var dayName;
@@ -28,25 +26,51 @@ $(function(){
       }
     }
   }
-  dayName = days[day];
-  $('#weekday').text(dayName);
-  $.get('psalms/'+day+'/psalm-verses.html',function(data){
-    var html = ant + psalm + data + ant;
-    html = html.replace(/{day}/g,day);
-    $('#placeholder').replaceWith(html);
-    updateGabc();
-  });
-  $('#in-manus-tuas-choices input,#te-lucis-choices input,#marian-antiphon-choices input,#confiteor-choices input').change(function(){
-    var chant = this.name;
-    var src = chant + '/' + this.value + '.gabc';
-    console.info(src);
-    $('#'+chant).attr('src',src);
+  var setPsalms = function(day,paschalTime) {
+    pt = paschalTime?'-PT':'';
+    var ant = paschalTime?
+      "<div class='jgabc' src='psalms/ant-PT.gabc'></div>" :
+      "<div class='jgabc' src='psalms/"+day+"/ant.gabc'></div>";
+    var psalm = "<div class='jgabc' src='psalms/"+day+"/psalm"+pt+".gabc'></div>";
+    dayName = days[day];
+    $('#weekday').text(dayName);
+    var gotData = function(data){
+      var html = ant + psalm + data + ant;
+      $('#placeholder').empty().append(html);
+      updateGabc();      
+      $('#in-manus-tuas-' + (paschalTime?'pt':'ordinary')).prop('checked',true).change();
+    };
+    $.get('psalms/'+day+'/psalm-verses'+pt+'.html',gotData).error(function(){
+      $.get('psalms/'+day+'/psalm-verses.html',gotData);
+    });
+  }
+  var pt = $.QueryString.pt && $.QueryString.pt!='0' && $.QueryString.pt!='false';
+  setPsalms(day,pt);
+  if(pt) {
+    $('#season-paschal').prop('checked',true);
+  }
+  var setChantSrc = function($div,src){
+    if(!$div || $div.length == 0) return;
     $.get(src,function(data){
-      $('#'+chant).text(data);
-      var old=$('#'+chant).next(".jgabc-svg").find("svg")[0];
+      $div.attr('src',src).text(data);
+      var old=$div.next(".jgabc-svg").find("svg")[0];
       if(!old) console.warn('Couldn\'t find svg to update.');
       else updateChant(data,old,true);
     });
+  };
+  $('[id$=-choices] input').change(function(){
+    var chant = this.name;
+    if(chant=='season') {
+      setPsalms(day,this.value=='paschal');
+      return;
+    }
+    var src = chant + '/' + this.value + '.gabc';
+    console.info(src);
+    setChantSrc($('#'+chant),src);
+    var $div = $('div.jgabc[' + this.id + ']');
+    if($div.length > 0) {
+      setChantSrc($div,$div.attr(this.id));
+    }
     $('div.' + chant).hide();
     $('div.' + chant + '.' + this.value).show();
   });
