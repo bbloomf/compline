354554b83b78ac838e6ee38b31b829156facc07f
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2016-05-31 10:42:27 -0400

    Removed jquery Added CDN fallback javascripts

---
+++ fallback/download-fallback-js.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -O https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js
+curl -O https://cdnjs.cloudflare.com/ajax/libs/document-register-element/0.5.3/document-register-element.js
+curl -O https://ex.surge.sh/exsurge.js
