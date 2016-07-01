d893dcd5c31506878b80a6f64d78eece211d5612
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2016-07-01 16:08:27 -0400

    Use service worker for offline caching

--- compline.js
+++ compline.js
@@ -459,4 +459,13 @@ $(function($){
   $('#date').change(function(){
     if(this.value) setDate(moment(this.value));
   }).change();
+  if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js').then(function(registration) {
+    // Registration was successful
+    // console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
 });
--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+var VERSION = '2016.07.01';
 var CACHE_NAME = 'compline-cache-v1';
 var urlsToCache = [
 './',
@@ -127,7 +128,6 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
   caches.open(CACHE_NAME)
     .then(function(cache) {
-      console.log('Opened cache');
       return cache.addAll(urlsToCache);
     })
   );
