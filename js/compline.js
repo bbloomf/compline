f3196bbbaaf5df8cd67e345683b33a13481e3c5f
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2016-07-17 08:02:29 -0400

    Moving things around to start using require.js

--- js/compline.js
+++ js/compline.js
@@ -1,3 +1,12 @@
+requirejs.config({
+  baseUrl: 'js/lib'
+});
+requirejs(['jquery','moment','calendar','chant-element'], function($,moment,calendar) {
+    //This function is called when scripts/helper/util.js is loaded.
+    //If util.js calls define(), then this function is not fired until
+    //util's dependencies have loaded, and the util argument will hold
+    //the module value for "helper/util".
+});
 $(function($){
   'use strict';
   var _currentRegion = '',
